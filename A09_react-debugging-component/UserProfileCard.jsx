61566b4ef6f4b428549fa41e034ef4d1f93effa3
Ydrey Ann Ramirez <ydreyannramirez035@gmail.com>
2026-07-24 16:20:05 +0800

    Done and completed Part C

--- A09_react-debugging-component/UserProfileCard.jsx
+++ A09_react-debugging-component/UserProfileCard.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 
 function UserProfileCard() {
   const userData = {
-    name: "Elmer Alvarado",
-    avatarUrl: "https://i.pravatar.cc/100", // sample avatar
-    bio: "Appdev1 instructor.",
-    skills: ["React", "JavaScript", "HTML", "CSS"],
+    name: "Ydrey Ann Ramirez",
+    avatarUrl:
+      "https://scontent.fmnl30-3.fna.fbcdn.net/v/t39.30808-1/525252326_1760934251463362_8743625505607814947_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s200x200&_nc_cat=107&_nc_map=urlgen_bucketless&ccb=1-7&_nc_sid=e99d92&_nc_eui2=AeHu292J6WkPnDrHfaEv0Q9dCPYfq4-KW8sI9h-rj4pby0SV3a3qccbPHQQ00ygCmutCEbZQ3bN8KDLXTotHM9U3&_nc_ohc=P_y6IFd0fkIQ7kNvwHPy38T&_nc_oc=AdptV6ALv_K2jsDwIsK2g12sEqbHB2HxMQAeje2kt8d8_YuTDKuBICMoHAKbE6shZJA&_nc_zt=24&_nc_ht=scontent.fmnl30-3.fna&_nc_gid=vV21yH0xRUeEa7JnayyJiA&_nc_ss=7b2a8&oh=00_AQDD0ZGYla7UgSrtMi7u5y4m8zsAr_j7qPW1r3s_D1r3wQ&oe=6A68F01B", // sample avatar
+    bio: "BSIS 3 student",
+    skills: ["Canva", "React", "JavaScript", "HTML", "CSS"],
     isOnline: true,
-    lastUpdated: "2 hours ago",
+    lastUpdated: "1 minute ago",
   };
 
   const [messageCount, setMessageCount] = useState(0);
+  const [isFavorited, setIsFavorited] = useState(false);
 
   function handleSendMessage() {
     setMessageCount(messageCount + 1);
@@ -18,6 +20,10 @@ function UserProfileCard() {
 
   function handleReset() {
     setMessageCount(0);
+  }
+
+  function handleFavoriteToggle() {
+    setIsFavorited((prev) => !prev);
   }
 
   return (
@@ -45,6 +51,12 @@ function UserProfileCard() {
 
         <button onClick={handleSendMessage}>Send Message</button>
         <button onClick={handleReset}>Reset</button>
+
+        {userData.isOnline && (
+          <button onClick={handleFavoriteToggle}>
+            {isFavorited ? "★ Favorited" : "☆ Favorite"}
+          </button>
+        )}
       </div>
       <p className="footer">Card last updated: {userData.lastUpdated}</p>
     </>
